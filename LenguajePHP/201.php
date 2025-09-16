cd7f75fc30bf20db2e6058ff77b22c107d6eb962
1DAW - Javier Fernandez  Huidobro <jbfernandezh01@iesvjp.es>
2025-09-16 20:04:25 +0200

    dsf

---
+++ LenguajePHP/201.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Muestra 3 frases, cada una en un párrafo utilizando las tres posibilidades que existen de mostrar contenido. Tras ello, introduce dos comentarios, uno de bloque y otro de una línea. -->
+</body>
+</html>
