9044206553eed342c18a3377639f80bccdc2180a
1DAW - Javier Fernandez  Huidobro <jbfernandezh01@iesvjp.es>
2025-09-16 20:15:08 +0200

    sdfg

---
+++ LenguajePHP/204.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- Es el mismo ejercicio que el anterior, pero separando la lógica. En el primer archivo crearemos el formulario para introducir los datos, y luego recogemos los datos y generamos la tabla en el segundo archivo. -->
+     <form action="204.php" method="get">
+        <p><label for="nombre">Introduce nombre</label></p>
+        <input type="text" name="nombre" id="nombre">
+        <p><label for="apellido1"></label></p>
+     </form>
+    <?php
+    
+    ?>
+</body>
+
+</html>
