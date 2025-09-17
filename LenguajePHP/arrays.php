e2224b2f1f7b283b354bd90a745acc72ea9e924e
1DAW - Javier Fernandez  Huidobro <jbfernandezh01@iesvjp.es>
2025-09-17 17:51:58 +0200

    php

--- LenguajePHP/arrays.php
+++ LenguajePHP/arrays.php
@@ -1,9 +1,10 @@
 <?php
 
-// Ejemplo de array
+echo "Ejemplo de array". "<br>";
 
 $vector = array("Manzana", "Banana", "Cereza");
-
+print_r($vector);
+echo "<br>";
 $vector2 = ["Rojo", "Verde", "Azul"];
 
 $vector3 = [];
@@ -11,8 +12,8 @@ $vector3[0] = "Perro";
 $vector3[1] = "Gato";
 $vector3[] = "Pájaro";
 
-echo $vector[1]; // Imprime "Banana"
-echo $vector2[0]; // Imprime "Rojo"
+echo $vector[1]. "<br>"; // Imprime "Banana"
+echo $vector2[0]. "<br>"; // Imprime "Rojo"
 echo $vector3[2]; // Imprime "Pájaro"
 
 ?>
