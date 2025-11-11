163e76e098755c930a891abebbe825a01071934a
1DAW - Javier Fernandez  Huidobro <jbfernandezh01@iesvjp.es>
2025-11-11 20:25:29 +0100

    monolog

---
+++ Monolog/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'monologos/holamonolog',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b9ca38c1f6198af65bb8dbfe0e8fe6822f5ea827',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'monologos/holamonolog' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b9ca38c1f6198af65bb8dbfe0e8fe6822f5ea827',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
