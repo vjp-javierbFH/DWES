3596b121fa11c4a871aa715c5b0c4cdb532f1b35
1DAW - Javier Fernandez  Huidobro <jbfernandezh01@iesvjp.es>
2025-11-12 20:17:25 +0100

    p

--- WebScrapping/src/531baloncesto.php
+++ WebScrapping/src/531baloncesto.php
@@ -4,9 +4,8 @@ require '../vendor/autoload.php';
 $httpClient = new \Goutte\Client();
 $response = $httpClient->request('GET', "https://www.seleccionbaloncesto.es/inicio.aspx");
 
-$alturas = []; 
-$response->filter('table#_ctl1_plantillaListView_contenedor.tabla-dataGrid tbody tr[id^="_Tr1"] td[id^="_alturaTd"]')->each(
+$response->filter('table#_ctl1_plantillaListView_contenedor.tabla-dataGrid tbody tr  td#_ctl1_plantillaListView_ctrl3_alturaTd')->each(
     function ($node) use (&$alturas){
-        $alturas[] = trim($node->text());
+        $alturas[] = $node->text();
     }
 );
