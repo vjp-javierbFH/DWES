5596d43341a2786b17a30b887366bef31f767fca
1DAW - Javier Fernandez  Huidobro <jbfernandezh01@iesvjp.es>
2025-11-12 20:17:31 +0100

    cfxb

--- GOUTTEPRUEBA/src/inicio.php
+++ GOUTTEPRUEBA/src/inicio.php
@@ -5,7 +5,7 @@ $httpClient = new \Goutte\Client();
 $response = $httpClient->request('GET', 'https://books.toscrape.com/catalogue/category/books/classics_6/index.html');
 print($response->html());
 
-$$response->filter('.row li article div.product_price p.price_color')->each(
+$response->filter('.row li article div.product_price p.price_color')->each(
     // le pasamos $precios por referencia para poder editarla dentro del closure
     function ($node) use (&$precios) {
         $precios[] = $node->text();
