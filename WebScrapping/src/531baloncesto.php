408b0b2a28a76d28481777491f7e9db632ac1ead
1DAW - Javier Fernandez  Huidobro <jbfernandezh01@iesvjp.es>
2025-11-12 20:00:47 +0100

    web

--- WebScrapping/src/531baloncesto.php
+++ WebScrapping/src/531baloncesto.php
@@ -5,16 +5,8 @@ $httpClient = new \Goutte\Client();
 $response = $httpClient->request('GET', "https://www.seleccionbaloncesto.es/inicio.aspx");
 
 $alturas = []; 
-// CAMBIO MÍNIMO: selector genérico que sí existe
-$response->filter('table tbody tr')->each(function ($row) use (&$alturas) {
-    $cells = $row->filter('td')->extract(['_text']);
-    if (count($cells) >= 3) {
-        $altura = trim($cells[2] ?? ''); // altura suele estar en la 3ª columna
-        if (preg_match('/\d[,.]\d{2}/', $altura)) {
-            $alturas[] = $altura;
-        }
+$response->filter('table#_ctl1_plantillaListView_contenedor.tabla-dataGrid tbody tr[id^="_Tr1"] td[id^="_alturaTd"]')->each(
+    function ($node) use (&$alturas){
+        $alturas[] = trim($node->text());
     }
-});
-
-// Mostrar alturas
-print_r($alturas);
+);
